b0a9aab326a3a6784d6167eee9c6d57ffb8faae1
Grace <lunay0325@gmail.com>
2019-03-12 23:54:21 +0800

    Resolve the dead lock by changing the order of lock

--- src/dead_lock/DeadLockTest.java
+++ src/dead_lock/DeadLockTest.java
@@ -6,8 +6,9 @@ class DeadLockTest {
         String firstChopsticks = "第一支筷子";
         String secondChopsticks = "第二支筷子";
 
+        //解决方法一：修改加锁的顺序。当两个线程加锁的顺序是一致的时候，不会出现死锁的情况。
         Thread t1 = new Thread(new DeadLockThread(firstChopsticks, secondChopsticks), "甲");
-        Thread t2 = new Thread(new DeadLockThread(secondChopsticks, firstChopsticks), "乙");
+        Thread t2 = new Thread(new DeadLockThread(firstChopsticks, secondChopsticks), "乙");
 
         t1.start();
         Thread.sleep(5000);
